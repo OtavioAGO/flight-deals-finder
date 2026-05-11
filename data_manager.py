3848d2fc5b4ecb5da38c7e9af69b5f28a2cf2ac7
Otavio Augusto <otavioago@gmail.com>
2026-05-11 19:11:47 -0300

    Data manager base

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,15 @@
+import os
+from requests_cache import CachedSession
+from dotenv import load_dotenv
+
+load_dotenv()
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.url = os.getenv('SHEETY_URL')
+        self.header = {'Authorization':f"Bearer {os.getenv('SHEETY_TOKEN')}"}
+        self.session = CachedSession('data_cache')
+
+    def get_data(self):
+        res = self.session.get(self.url, self.header)
+        return res.json()
+    
